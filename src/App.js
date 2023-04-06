b20d75aa9c090d35ea15ee81035229bb6dd85ab3
NickMcKendry <nickmckendry@gmail.com>
2023-04-06 17:04:19 -0600

    Basic setup and search bar logging the query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import SearchBar from "./Components/SearchBar";
 
 function App() {
+  const handleSearch = (query) => {
+    console.log("Search query:", query);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Album Recommender</h1>
+      <SearchBar onSearch={handleSearch} />
     </div>
   );
 }
